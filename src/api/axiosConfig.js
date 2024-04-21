af0fc749079a28ec99085b0a93dd8db847efedc5
yuito-syz <yuito22suto@gmail.com>
2024-04-21 20:02:03 +0900

    setting ngrok and axios

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL,
-    headers
-})
+    baseURL: 'https://4ba2-123-0-94-84.ngrok-free.app',
+    headers: {"ngrok-skip-browser-warning": "true"}
+});
